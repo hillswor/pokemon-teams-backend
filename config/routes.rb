6914ffe13ad48f7e2a1a66fe651db40b3b09f776
Bruce Hillsworth <brucehillsworth@Air.local>
2020-02-12 16:36:42 -0700

    added serializers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :pokemons
   resources :trainers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/pokemons', to: 'pokemons#index'
+  get '/pokemons/:id', to: 'pokemons#show'
+  get '/trainers', to: 'trainers#index'
+  get '/trainers/:id', to: 'trainers#show'
 end
